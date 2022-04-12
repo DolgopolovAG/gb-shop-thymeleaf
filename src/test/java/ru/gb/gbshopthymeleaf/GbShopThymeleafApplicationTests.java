65a5d6502f82024847109543ca2bea7b35a629c5
Dolgopolov <Dolgopolov@it-limon.ru>
2022-04-12 14:42:46 +0500

    base add

---
+++ src/test/java/ru/gb/gbshopthymeleaf/GbShopThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.gbshopthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbShopThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
